6906c6358244bd85b83feb0e58a531ecd49e393e
Adrian Rivas <arivas@ako.com>
2019-10-08 21:45:08 +0200

    seq.json

--- configure_database.sh
+++ configure_database.sh
@@ -6,6 +6,11 @@ wget -O service_catalog.json http://raw.githubusercontent.com/adrianARL/database
 mongoimport --db=globalDB --collection=service_catalog --file=service_catalog.json
 rm service_catalog.json
 
+
+echo '{"_id":"nodeID","seq":0}' > seq.json
+mongoimport --db=globalDB --collection=nodes --file=seq.json
+rm seq.json
+
 wget -O configure_cloud_agent.py http://raw.githubusercontent.com/adrianARL/database_configuration/master/configure_cloud_agent.py
 python3 configure_cloud_agent.py
 mongoimport --db=globalDB --collection=nodes --file=device.config
